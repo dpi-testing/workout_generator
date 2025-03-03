0273acbadc3ee0ad4bc1beac073ccf816ce18591
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-03-03 22:31:19 +0000

    set api key in .env file

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 require "json"
+require "dotenv"
+Dotenv.load
+
 
 get("/") do
   erb(:index)
@@ -33,12 +36,17 @@ get("/random_workouts") do
   api_key = ENV["API_KEY"].to_s.gsub("\n", "").strip
 
   response = HTTP.headers("X-Api-Key" => api_key).get(uri)
+  puts "API Response: #{response.to_s}"
+
 
   workouts = JSON.parse(response.to_s)
-  if workouts.any?
-    workout = workouts.first
+  puts workouts.inspect # Debug the response
 
+  if workouts.any? && workouts.first.is_a?(Hash)
+    workout = workouts.first
     @workout_details = "Name: #{workout["name"]}\nType: #{workout["type"]}\nMuscle: #{workout["muscle"]}\nEquipment: #{workout["equipment"]}\nDifficulty: #{workout["difficulty"]}\nInstructions: #{workout["instructions"]}"
+  else
+    @workout_details = "No valid workout data available."
   end
   @workouts = workouts
   erb(:random)
